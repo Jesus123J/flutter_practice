b6717d31ef3893c3065ff8028afe2db3eba81b7a
Thiago <gutierrezdelatorrej30@gmail.com>
2024-09-23 08:55:57 -0500

    proyect _ fluter

---
+++ lib/app/controller/ControllerDashboard.dart
@@ -0,0 +1,18 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class ControllerDashboard extends StatefulWidget{
+
+
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
