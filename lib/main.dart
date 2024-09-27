caf4a423c0d83a111aed75810f75251e527dd798
Thiago <gutierrezdelatorrej30@gmail.com>
2024-09-26 23:19:29 -0500

    ajouter du code à mon application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _dashboardPage();
+
+}
+
+class _dashboardPage extends State<MyApp>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+          backgroundColor: Colors.brown,
+       ),
+    );
+  }
+}
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
